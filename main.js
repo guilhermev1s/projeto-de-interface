685c91ba18542c05c92833af11b59f7bf046b48f
Guilherme <guivs56@gmail.com>
2023-05-19 20:32:59 -0300

    19/05

---
+++ main.js
@@ -0,0 +1,13 @@
+//Inicio
+alert("Seja bem vindo");
+//variaveis e while
+var contador = 0;
+while(contador < 2) {
+    alert("Oi");
+    contador = contador + 1;
+
+}
+//input
+var nome;
+nome = prompt("qual seu nome?");
+alert("oi, " + nome);
